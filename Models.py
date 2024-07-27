5784025717dc9b5ba1f321b1d633ab26fba1758d
Vijay Paruchuri <gamebo11@duck.com>
2024-07-27 15:23:39 -0400

    Added NiN model to Models.py file

--- Models.py
+++ Models.py
@@ -19,3 +19,30 @@ class LeNet(nn.Module):
     def forward(self, X):
         logits = self.net(X)
         return logits
+    
+    
+def nin_block(out_channels, kernel_size, stride, padding):
+    return nn.Sequential(
+        nn.LazyConv2d(out_channels, kernel_size=kernel_size, stride=stride, padding=padding), nn.ReLU(),
+        nn.LazyConv2d(out_channels, kernel_size=1), nn.ReLU(),
+        nn.LazyConv2d(out_channels, kernel_size=1), nn.ReLU()
+    )
+    
+class NiN(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.net = nn.Sequential(
+            nin_block(96, kernel_size=11, stride=4, padding=0),
+            nn.MaxPool2d(3, stride=2),
+            nin_block(256, kernel_size=5, stride=1, padding=2),
+            nn.MaxPool2d(3, stride=2),
+            nin_block(384, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout(0.5),
+            nin_block(num_classes, kernel_size=3, stride=1, padding=1),
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten()            
+        )
+    def forward(self, X):
+        logits = self.net(X)
+        return logits
